4f20735a7c27f0b86c04a49a5bd010cafea66bb8
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-18 10:04:07 -0700

    Create method and test for author to write book

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,20 @@
+# require 'book'
+
 class Author
+  attr_reader :name, :books
+
+  def initialize(author_info)
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+    @books = []
+  end
+
+  def name
+    @name = "#{@first_name} #{@last_name}"
+  end
+
+  def write(title, date)
+    Book.new({title: title, publication_date: date})
+  end
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -7,31 +7,25 @@ class AuthorTest < Minitest::Test
 
   def setup
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
   end
 
   def test_it_has_attributes
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
-
+    assert_equal [], @charlotte_bronte.books
   end
+
+  def test_an_author_can_write_a_book
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
 
 end
 
 
-#
-# pry(main)> charlotte_bronte.name
-# #=> "Charlotte Bronte"
-#
-# pry(main)> charlotte_bronte.books
-# #=> []
-#
-# pry(main)> jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-# #=> #<Book:0x00007fb896e22538...>
-#
-# pry(main)> jane_eyre.class
-# #=> Book
-#
-# pry(main)> jane_eyre.title
-# #=> "Jane Eyre"
+
 #
 # pry(main)> charlotte_bronte.write("Villette", "1853")
 # #=> #<Book:0x00007fb8980aaca0...>
