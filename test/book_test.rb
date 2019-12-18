3dae7a7186b6235a0dd252ca8f90838cc79c05a7
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-18 09:24:03 -0700

    Iteration 1 book and book test complete

--- test/book_test.rb
+++ test/book_test.rb
@@ -10,7 +10,8 @@ class BookTest < Minitest::Test
 
 
   def test_it_has_attributes
-    # assert
+    assert_equal "To Kill a Mockingbird", @book.title
+    assert_equal "Harper Lee", @book.author
+    assert_equal "1960", @book.publication_year
   end
-
 end
